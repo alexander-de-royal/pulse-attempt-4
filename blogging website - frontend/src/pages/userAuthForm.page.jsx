5a085c7df96b280a8d4f181a53a9493b5377bfd7
Alexander Royal <alexander.de.royal@gmail.com>
2023-12-27 19:30:42 +0545

    page animation completed

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, {Fragment} from "react";
+import googleIcon from "../imgs/google.png";
 import InputBox from "../components/input.component";
+import {Link} from "react-router-dom";
+import AnimationWrapper from "../common/page-animation.jsx";
 
 const UserAuthForm = ({type}) => {
     return (
+        <AnimationWrapper keyValue={type}>
         <section className="h-cover flex items-center justify-center">
             <form className="w-[80%] max-w-[400px]">
                 <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
@@ -17,24 +21,55 @@ const UserAuthForm = ({type}) => {
                         icon="fi-rr-user"
                     />
                     : ""}
-                    <InputBox
-                        name="email"
-                        type="email"
-                        placeholder="Email"
-                        icon="fi-rr-envelope"
-                    />
-                    <InputBox
-                        name="password"
-                        type="password"
-                        placeholder="Password"
-                        icon="fi-rr-key"
-                    />
+                <InputBox
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    icon="fi-rr-envelope"
+                />
+                <InputBox
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    icon="fi-rr-key"
+                />
 
-                <button className="btn-dark center mt-14">
+                <button className="btn-dark center mt-14" type="submit">
                     {type.replace("-", " ")}
                 </button>
+
+                <div
+                    className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+                    <hr className="w-1/2 border-black"/>
+                    <p>or</p>
+                    <hr className="w-1/2 border-black"/>
+                </div>
+
+                <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
+                    <img src={googleIcon} className="w-5 "/>
+                    Continue With Google
+                </button>
+
+                {
+                    type === "sign-in"
+                        ?
+                        <p className="mt-6 text-dark-grey text-xl text-center">
+                            Don't have an account ?
+                            <Link to="/signup" className="underline text-black text-xl ml-1">
+                                Join Us Today
+                            </Link>
+                        </p>
+                        :
+                        <p className="mt-6 text-dark-grey text-xl text-center">
+                            Already a member ?
+                            <Link to="/signin" className="underline text-black text-xl ml-1">
+                                Sign in here
+                            </Link>
+                        </p>
+                }
             </form>
         </section>
+        </AnimationWrapper>
     );
 }
 
